388e5e49084dec85a0fb19c92589381ab02ec310
Lenorar <lenorarigoni@Lenoras-MacBook-Pro.local>
2017-07-20 13:02:21 -0400

    changing card to an object and applying methods

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,57 @@
 
 
-console.log("Up and running!");
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts", 
+		cardImage: "images/queen-of-hearts.png" 
+	},
 
-var cards = ["queen", "queen", "king", "king"]
+	{
+		rank: "queen",
+		suit: "diamonds", 
+		cardImage: "images/queen-of-diamonds.png" 
+	},
+
+	{
+		rank: "king",
+		suit: "hearts", 
+		cardImage: "images/king-of-hearts.png" 
+	},
+
+	{
+		rank: "king",
+		suit: "diamonds", 
+		cardImage: "images/king-of-diamonds.png" 
+	}
+
+];
+
+
+
 
 var cardsInPlay = []
 
-var cardOne = cards[0]
 
-cardsInPlay.push(cardOne);
+var flipCard = function(cardId){
 
-console.log("User flipped queen");
+	console.log("User flipped " + cards[cardId].rank);
 
-var cardTwo = cards[2]
+	cardsInPlay.push(cards[cardId].rank);
 
-cardsInPlay.push(cardTwo);
+	console.log(cards[cardId].cardImage)
 
-console.log("User flipped king");
+	console.log(cards[cardId].suit)
+	
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-};
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[2]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+	};
+}
+
+flipCard(0);
+flipCard(2);
